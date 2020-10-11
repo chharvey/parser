3c92dbefa2be2e0b4943e896f40e32130a5cd7af
Chris Harvey <chrisharvey2pi@gmail.com>
2020-10-10 23:51:53 -0400

    update tests

--- test/helpers.ts
+++ test/helpers.ts
@@ -1,3 +1,7 @@
+import * as assert from 'assert'
+
+
+
 /**
  * Return the last index of an iterable.
  * @param   iter an iterable
@@ -16,4 +20,28 @@ export function lastIndex(iter: { length: number }): number {
  */
 export function lastItem(iter: any[] | string): any {
 	return iter[lastIndex(iter)];
+}
+
+
+
+/**
+ * Assert the length of an array or tuple.
+ * Useful helper for determining types of items in heterogeneous tuples.
+ * @typeParam T   the type of items in the array/tuple
+ * @param array   the array or tuple to test
+ * @param length  the length to assert
+ * @param message the message to send into {@link assert.strictEqual}
+ */
+export function assert_arrayLength<T>(array: readonly T[], length: 0      , message?: string | Error): asserts array is readonly [];
+export function assert_arrayLength<T>(array: readonly T[], length: 1      , message?: string | Error): asserts array is readonly [T];
+export function assert_arrayLength<T>(array: readonly T[], length: 2      , message?: string | Error): asserts array is readonly [T, T];
+export function assert_arrayLength<T>(array: readonly T[], length: 3      , message?: string | Error): asserts array is readonly [T, T, T];
+export function assert_arrayLength<T>(array: readonly T[], length: 4      , message?: string | Error): asserts array is readonly [T, T, T, T];
+export function assert_arrayLength<T>(array: readonly T[], length: 5      , message?: string | Error): asserts array is readonly [T, T, T, T, T];
+export function assert_arrayLength<T>(array: readonly T[], length: 6      , message?: string | Error): asserts array is readonly [T, T, T, T, T, T];
+export function assert_arrayLength<T>(array: readonly T[], length: 7      , message?: string | Error): asserts array is readonly [T, T, T, T, T, T, T];
+export function assert_arrayLength<T>(array: readonly T[], length: 8      , message?: string | Error): asserts array is readonly [T, T, T, T, T, T, T, T];
+export function assert_arrayLength<T>(array: readonly T[], length: number , message?: string | Error): void;
+export function assert_arrayLength<T>(array: readonly T[], length: number , message?: string | Error): void {
+	return assert.strictEqual(array.length, length, message)
 }
--- test/parser/Parser.test.ts
+++ test/parser/Parser.test.ts
@@ -1,6 +1,8 @@
 import * as assert from 'assert';
 
+import {Filebound} from '../../src/utils';
 import {
+	Token,
 	TokenFilebound,
 } from '../../src/lexer/Token';
 import type {ParseNode} from '../../src/parser/ParseNode';
@@ -8,7 +10,12 @@ import {
 	ParseError01,
 } from '../../src/error/ParseError';
 import {
+	assert_arrayLength,
+} from '../helpers';
+import {
 	ParserSample,
+	ParseNodeUnit,
+	ParseNodeGoal,
 } from '../sample/';
 
 
@@ -25,6 +32,45 @@ describe('Parser', () => {
 
 		it('rejects unexpected tokens.', () => {
 			assert.throws(() => new ParserSample(`(+ 3 4 5)`).parse(), ParseError01);
+		});
+
+		describe('ParserSample', () => {
+			specify('Goal ::= #x02 Unit #x03;', () => {
+				const goal: ParseNode = new ParserSample(`(+ (* 2 3) 5)`).parse();
+				/*
+					<Goal>
+						<FILEBOUND>␂</FILEBOUND>
+						<Unit src="(+ (* 2 3) 5)">...</Unit>
+						<FILEBOUND>␃</FILEBOUND>
+					</Goal>
+				*/
+				assert.ok(goal instanceof ParseNodeGoal);
+				assert_arrayLength(goal.children, 3, 'goal should have 3 children');
+				const [sot, unit, eot]: readonly [Token, ParseNodeUnit, Token] = goal.children;
+				assert.deepStrictEqual(
+					[sot.source,    unit.source,         eot.source],
+					[Filebound.SOT, `( + ( * 2 3 ) 5 )`, Filebound.EOT],
+				);
+			});
+
+			specify('Unit ::= "(" OPERATOR Unit Unit ")";', () => {
+				const unit: ParseNodeUnit = new ParserSample(`(+ (* 2 3) 5)`).parse().children[1] as ParseNodeUnit;
+				/*
+					<Unit>
+						<PUNCTUATOR>(</PUNCTUATOR>
+						<OPERATOR>+</OPERATOR>
+						<Unit src="(* 2 3)">...</Unit>
+						<Unit src="5">...</Unit>
+						<PUNCTUATOR>)</PUNCTUATOR>
+					</Unit>
+				*/
+				assert_arrayLength(unit.children, 5, 'unit should have 5 children');
+				const [open, op, left, right, close]: readonly [Token, Token, ParseNodeUnit, ParseNodeUnit, Token] = unit.children;
+				assert.deepStrictEqual(
+					[open.source, op.source, left.source, right.source, close.source],
+					[`(`,         `+`,       `( * 2 3 )`, `5`,          `)`],
+				);
+			});
 		});
 	});
 });
